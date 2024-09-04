f16763f279e0aa304c2e285f6b2c6c1e6ff66391
Alireza Azimi <alireza@vega.cs.ualberta.ca>
2024-09-04 14:37:40 -0600

    save progress

--- non_visual_reacher.py
+++ non_visual_reacher.py
@@ -9,21 +9,24 @@ import gymnasium as gym
 from gymnasium import spaces
 import glfw
 import argparse
-WIDTH = 500
-HEIGHT = 500
 
+ARM_VEL_LIMITS = np.array([2.61799, 2.61799, 2.61799, 2.61799, 3.14159, 3.14159, 3.14159, 0])
 class FrankaPandaEnv(gym.Env):
-    def __init__(self, render=False, manual_mode = False):
+    def __init__(self, render=False, manual_mode = False, frame_skip=20):
         super(FrankaPandaEnv, self).__init__()
         
         # Load the Franka Panda model from MuJoCo Menagerie
         model_path = "env.xml"
         self.model = mujoco.MjModel.from_xml_path(model_path)
         self.data = mujoco.MjData(self.model)
-
+        self._frame_skip = frame_skip
+        self._dt = self.model.opt.timestep * self._frame_skip
         # Define the action and observation spaces
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(self.model.nu,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.model.nq + self.model.nv,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3 + 3 + self.model.nq + self.model.nv,), dtype=np.float32)
+        self._last_qpos = self.data.qpos.copy()
+        self._arm_ctrlrange_min = self.model.actuator_ctrlrange[:, 0]
+        self._arm_ctrlrange_max = self.model.actuator_ctrlrange[:, 1] 
         self.view = self._render_env(not manual_mode) if render else None
         
     def reset(self):
@@ -31,51 +34,70 @@ class FrankaPandaEnv(gym.Env):
         self.data.qpos = default_kf.qpos.copy()
         self.data.ctrl = default_kf.ctrl.copy()
         ## Change Target Position
-        self.model.site_pos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')] = [ np.random.uniform(0.5, 0.8),
-                                                                                                          np.random.uniform(0, 0.3),
-                                                                                                          np.random.uniform(0.5, 0.8)]
+        self.model.site_pos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')] = [ np.random.uniform(0.7, 0.8),
+                                                                                                          np.random.uniform(-0.2, 0.2),
+                                                                                                          np.random.uniform(0.2, 0.6)]
         mujoco.mj_forward(self.model, self.data)
         self._sync_view()
         return self._get_obs()
     
     def step(self, action):
-        self.data.ctrl[:] = action * 0.2
-        mujoco.mj_step(self.model, self.data, 20)
+        action = np.clip(action, -1.0, 1.0) 
+        # print(action.shape)
+        action = (((action + 1) * 0.5) * ARM_VEL_LIMITS * 2) - ARM_VEL_LIMITS
+        action = action * self._dt
+        action *= 0.4 #np.clip(self._compute_distance(), 0, 0.4) # Scale down the action
+        
+        new_qpos = self._last_qpos[:8] + action
+        clipped_new_qpos = np.clip(new_qpos, self._arm_ctrlrange_min, self._arm_ctrlrange_max)
+        
+        self.data.ctrl[:] = clipped_new_qpos
+        mujoco.mj_step(self.model, self.data, self._frame_skip)
+
         obs = self._get_obs()
-        reward = self._compute_reward()
+        reward = self._compute_reward(action)
         done = self._is_done()
         info = {}
         self._sync_view()
+        self._last_qpos = self.data.qpos.copy()
         return obs, reward, done, info
 
     def _render_env(self, disable_panels=True):
         return mujoco.viewer.launch_passive(self.model, self.data, show_left_ui= not disable_panels, show_right_ui=not disable_panels)
+    
     def _sync_view(self):
         if self.view and self.view.is_running():
             with self.view.lock():
                 self.view.sync()
     
     def _get_obs(self):
-        return np.concatenate([self.data.qpos, self.data.qvel])
-    
-    def _compute_reward(self):
-        # Define a simple reward function
         robotic_arm_pointer = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'end_effector')]
         target = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')]
+        return np.concatenate([target, target - robotic_arm_pointer, self.data.qpos, self.data.qvel])
+    
+    def _compute_reward(self, action):
 
         # Calculate the distance
-        distance = np.linalg.norm(target - robotic_arm_pointer)
-        return -1 * distance * 10 - (10 if self._is_out_of_bounds(robotic_arm_pointer) else 0) # Extra penalty for going out of bounds
+        distance = self._compute_distance()
+
+        # calculate the action norm
+        action_norm = np.linalg.norm(action)
+
+        return -distance - action_norm # Extra penalty for doing drastic actions
     
     def _is_done(self):
-        robotic_arm_pointer = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'end_effector')]
-        target = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')]
-        distance = np.linalg.norm(target - robotic_arm_pointer)
-        return distance < 0.05 or self._is_out_of_bounds(robotic_arm_pointer)
+        # robotic_arm_pointer = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'end_effector')]
+        # target = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')]
+        # distance = np.linalg.norm(target - robotic_arm_pointer)
+        return False
     def _is_out_of_bounds(self, robotic_arm_pointer) -> bool:
         return not ( (0.1 < robotic_arm_pointer[2]  < 0.9) and\
                      (-0.4 < robotic_arm_pointer[1] < 0.4) and\
                      (0.1 < robotic_arm_pointer[0] < 0.9)) 
+    def _compute_distance(self):
+        robotic_arm_pointer = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'end_effector')]
+        target = self.data.site_xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, 'spherical_site')]
+        return np.linalg.norm(target - robotic_arm_pointer)
     
 
 def test_env():
@@ -95,11 +117,12 @@ def test_env():
 def run_manual():
     
     env = FrankaPandaEnv(render=True, manual_mode=True)
-    obs = env.reset()
+    # obs = env.reset()
     while env.view.is_running():
         mujoco.mj_step(env.model, env.data, 20)
-        print(f"Reward: {env._compute_reward()}, Done: {env._is_done()}")
+
         env._sync_view()
+
 
 if __name__ == "__main__":
 # Create an instance of the environment and test it
--- task_non_visual_franka.py
+++ task_non_visual_franka.py
@@ -18,6 +18,7 @@ from jsac.helpers.utils import MODE, make_dir, set_seed_everywhere, WrappedEnv
 import argparse
 import shutil
 
+
 config = {
     'conv': [
         # in_channel, out_channel, kernel_size, stride
@@ -50,14 +51,14 @@ def parse_args():
     
     # train
     parser.add_argument('--init_steps', default=10000, type=int)
-    parser.add_argument('--env_steps', default=1_000_000, type=int)
+    parser.add_argument('--env_steps', default=5_000_000, type=int)
     parser.add_argument('--batch_size', default=256, type=int)
     parser.add_argument('--sync_mode', default=True, action='store_true')
     parser.add_argument('--apply_rad', default=True, action='store_true')
     parser.add_argument('--rad_offset', default=0.01, type=float)
     
     # critic
-    parser.add_argument('--critic_lr', default=3e-4, type=float)
+    parser.add_argument('--critic_lr', default=3e-5, type=float)
     parser.add_argument('--critic_tau', default=0.01, type=float)
     parser.add_argument('--clip_global_norm', default=1.0, type=float)
     parser.add_argument('--critic_target_update_freq', default=1, type=int)
@@ -76,7 +77,7 @@ def parse_args():
     parser.add_argument('--temp_lr', default=1e-4, type=float)
     
     # misc
-    parser.add_argument('--update_every', default=2, type=int)
+    parser.add_argument('--update_every', default=1, type=int)
     parser.add_argument('--work_dir', default='.', type=str)
     parser.add_argument('--save_tensorboard', default=False, 
                         action='store_true')
@@ -84,23 +85,24 @@ def parse_args():
     parser.add_argument('--save_wandb', default=False, action='store_true')
 
     parser.add_argument('--save_model', default=True, action='store_true')
-    parser.add_argument('--save_model_freq', default=20000, type=int)
+    parser.add_argument('--save_model_freq', default=100_000, type=int)
     parser.add_argument('--load_model', default=-1, type=int)
     parser.add_argument('--start_step', default=0, type=int)
     parser.add_argument('--start_episode', default=0, type=int)
 
-    parser.add_argument('--rb_type', default='sm_buffer', type=str) # Options: se_buffer, sm_buffer
-    parser.add_argument('--rb_num_workers', default=16, type=int)   # Number of workers for se_buffer
     parser.add_argument('--buffer_save_path', default='', type=str) # ./buffers/
     parser.add_argument('--buffer_load_path', default='', type=str) # ./buffers/
+
+    # render
+    parser.add_argument('--render_env', default=False, type=bool)
     args = parser.parse_args()
     return args
 
-if __name__=="__main__":
-    args = parse_args()
-    env = FrankaPandaEnv(render=True)
+
+def run_training(args):
+    env = FrankaPandaEnv(render=args.render_env)
     env = WrappedEnv(env, start_step= args.start_step, 
-                     start_episode=args.start_episode)
+                     start_episode=args.start_episode, episode_max_steps=1000)
     
     args.work_dir += f'/results/{args.name}/seed_{args.seed}/'
 
@@ -147,18 +149,20 @@ if __name__=="__main__":
     args.proprioception_shape = env.observation_space.shape
     args.action_shape = env.action_space.shape
     args.env_action_space = env.action_space
-    args.image_shape = (0,0,0)
+    args.image_shape = None
     args.net_params = config
     agent = SACRADAgent(vars(args))
     obs = env.reset()
     first_step = True
     task_start_time = time.time()
+
     while env.total_steps < args.env_steps:
-        # pass
         t1 = time.time()
         action = agent.sample_actions(obs)
         t2 = time.time()
         next_obs, reward, done, info = env.step(action)
+        # truncate = env.total_steps % max_episode_length == 0
+        # done = done or truncate
         t3 = time.time()
         if not done:
             mask = 1.0
@@ -167,17 +171,22 @@ if __name__=="__main__":
         agent.add(obs, action, reward, next_obs, mask, first_step)
         obs = next_obs
         first_step = False
+        
         if done:
+            
             obs = env.reset()
             info['tag'] = 'train'
             info['elapsed_time'] = time.time() - task_start_time
             info['dump'] = True
             L.push(info)
             first_step = True
-        if env.total_steps > args.init_steps and env.total_steps % args.update_every==0:
+        if env.total_steps > args.init_steps and env.total_steps % args.update_every == 0:
 
             update_infos = agent.update()
+            
             if update_infos is not None:
+
+
                 for update_info in update_infos:
                     update_info['action_sample_time'] = (t2 - t1) * 1000
                     update_info['env_time'] = (t3 - t2) * 1000
@@ -186,6 +195,7 @@ if __name__=="__main__":
                     update_info['dump'] = False
 
                     L.push(update_info)
+                    # exit(0)
 
         if env.total_steps % args.xtick == 0:
             L.plot()
@@ -203,4 +213,66 @@ if __name__=="__main__":
     end_time = time.time()
     print(f'\nFinished in {end_time - task_start_time}s')
 
+def run_policy(args):
+    env = FrankaPandaEnv(render=True)
+    env = WrappedEnv(env, start_step= args.start_step, 
+                     start_episode=args.start_episode, episode_max_steps=1000)
+    
+    args.work_dir += f'/results/{args.name}/seed_{args.seed}/'
+
+    if os.path.exists(os.path.abspath(args.work_dir)):
+        print("loading model from work directory")
+    else:
+        exit(0)
+
+    make_dir(args.work_dir)
+
+    if args.buffer_save_path:
+        if args.buffer_save_path == ".":
+            args.buffer_save_path = os.path.join(args.work_dir, 'buffers')
+        make_dir(args.buffer_save_path)
+
+    L = Logger(args.work_dir, args.xtick, vars(args), 
+                   args.save_tensorboard, args.save_wandb)
+    
+    if args.buffer_load_path == ".":
+        args.buffer_load_path = os.path.join(args.work_dir, 'buffers')
+
+    args.model_dir = os.path.join(args.work_dir, 'checkpoints') 
+    if args.save_model:
+        make_dir(args.model_dir)
+
+    proprioception_shape = env.observation_space.shape
+    action_shape = env.action_space.shape
+    env_action_space = env.action_space
+
+    print(f"{proprioception_shape}, {action_shape}, {env_action_space}")
+    set_seed_everywhere(seed=args.seed)
+    
+    # args.single_image_shape = (args.image_width, args.image_height, 3)
+    args.proprioception_shape = env.observation_space.shape
+    args.action_shape = env.action_space.shape
+    args.env_action_space = env.action_space
+    args.image_shape = None
+    args.net_params = config
+    agent = SACRADAgent(vars(args))
+    obs = env.reset()
+    while True:
+        action = agent.sample_actions(obs, deterministic=True)
+        next_obs, reward, done, info = env.step(action)
+        obs = next_obs
+        if done:
+            obs = env.reset()
+            time.sleep(2)
+            print("Episode Done")
+            
+    
+
+if __name__=="__main__":
+    args = parse_args()
+    if args.load_model < 0:
+        run_training(args)
+    else:
+        run_policy(args)
+    
     
